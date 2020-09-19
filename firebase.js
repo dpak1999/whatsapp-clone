90b775205c1da470b376c6eb679ff45374a39ec7
dpak1999 <deepakkumardash313@gmail.com>
2020-09-19 20:57:18 +0530

    setting up project & firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWcQaZsFCBOEuSZcK5KQQGWxTdikmKr3E",
+  authDomain: "dpak-whatsapp.firebaseapp.com",
+  databaseURL: "https://dpak-whatsapp.firebaseio.com",
+  projectId: "dpak-whatsapp",
+  storageBucket: "dpak-whatsapp.appspot.com",
+  messagingSenderId: "426656017763",
+  appId: "1:426656017763:web:aae9f74564c20d05b1b756",
+};
