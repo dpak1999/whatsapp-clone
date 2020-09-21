ba4fdc5be91cb8b768d5cb606e7301cbdf9ff225
dpak1999 <deepakkumardash313@gmail.com>
2020-09-21 19:17:41 +0530

    creating sidebar & chat components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Chat from "./components/Chat";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Whatsapp build in progress</h1>
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
