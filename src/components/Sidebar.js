34ccd4e83de9ae36569a0253f5c1a3741a8313e6
dpak1999 <deepakkumardash313@gmail.com>
2020-09-21 21:29:23 +0530

    creating input outline and styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,37 @@
 import React from "react";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a side bar</h1>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats"></div>
     </div>
   );
 }
