1cddec891e0cdac18bfd427ef7913fbcb8b74994
dpak1999 <deepakkumardash313@gmail.com>
2020-09-22 21:33:16 +0530

    creating chat body and styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,58 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from '@material-ui/icons/Mic';
 import React from "react";
 import "./Chat.css";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am chat component</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Deepak</span>This is a message
+          <span className="chat__timestamp">{Date().toString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Sony</span>This is a message
+          <span className="chat__timestamp">{Date().toString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Deepak</span>This is a message
+          <span className="chat__timestamp">{Date().toString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form action="">
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
