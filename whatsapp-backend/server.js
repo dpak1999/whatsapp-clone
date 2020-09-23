f5ed5eb438c3a9e3dac222de5ac6e9297fed4e05
dpak1999 <deepakkumardash313@gmail.com>
2020-09-23 21:38:21 +0530

    creating db schema

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,16 +1,49 @@
 // imports
 import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // app configurations
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware
+app.use(express.json());
 
 // Database
+const connection_url =
+  "mongodb+srv://dpak:uPNGFbWT8BPZ7tSp@cluster0.lbokn.mongodb.net/whatsapp_db?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // routes
 app.get("/", (req, res) => res.status(200).send("Hello there"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 // server
 app.listen(port, () =>
