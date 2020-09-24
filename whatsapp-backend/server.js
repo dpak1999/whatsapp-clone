19fc9a729435dc534ab956d55d4fdf1d28a400df
dpak1999 <deepakkumardash313@gmail.com>
2020-09-24 20:48:55 +0530

    allowing access header to all

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -18,6 +18,12 @@ const pusher = new Pusher({
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 // Database
 const connection_url =
   "mongodb+srv://dpak:uPNGFbWT8BPZ7tSp@cluster0.lbokn.mongodb.net/whatsapp_db?retryWrites=true&w=majority";
