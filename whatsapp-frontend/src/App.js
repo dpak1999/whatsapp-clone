bcaa782a380d7250bfeae917cd933fc6399353e3
dpak1999 <deepakkumardash313@gmail.com>
2020-09-24 21:32:52 +0530

    adding and configuring axios for data fetching

--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -1,20 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   useEffect(() => {
     const pusher = new Pusher("0023aaa186da25c2db05", {
       cluster: "ap2",
     });
 
     const channel = pusher.subscribe("messages");
-    channel.bind("inserted", (data) => {
-      alert(JSON.stringify(data));
+    channel.bind("inserted", (newMessages) => {
+      alert(JSON.stringify(newMessages));
+      setMessages([...messages, newMessages]);
     });
-  }, []);
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
